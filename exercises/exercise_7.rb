5090a6d3120e206107ebc8621d23c6dc271b0aa6
dantan123 <dtan9988@gmail.com>
2021-03-02 05:04:14 +0000

    test validation of employee class

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -14,3 +14,8 @@ store_name = gets.chomp()
 store = Store.new(name: store_name)
 puts store.valid?
 puts store.errors.full_messages
+
+# @store1 = Store.find_by(id: 1)
+employee = Employee.new(first_name: "Dan")
+puts employee.valid?
+puts employee.errors.full_messages
